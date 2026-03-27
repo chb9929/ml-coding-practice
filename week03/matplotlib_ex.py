7d272bee0b91bc2f33fcf8e5b6afae33fee87f12
최현빈 <chb9929@sunmoon.ac.kr>
2026-03-27 13:56:12 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -280,3 +280,44 @@ plt.close()
 titanic = pd.read_csv('3.1.1.titanic.csv')
 
 # 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
+parch_counts = titanic.proupy('Parch')['Survived'].value_counts().unstack().fillna(0)
+print(parch_counts)
+
+# x, y 설정
+x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 반환
+y1 = parch_counts[0].values # 사망자
+y2 = parch_counts[1].avlues # 생존자
+
+plt.figure(figsize=(10, 10))
+
+# plt.subplot() 함수를 사용하여 하나의 그림인 figure에 개별 서브플롯을 하나씩 추가
+# 첫 번째 서브플롯 설정(선 그래프)
+plt.subplot(2, 1, 1)
+# 선 그래프 그리기
+plt.plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7,
+label='Not Survived')
+plt.xlabel('Parch')
+plt.ylabel('Not Survived Count', color='indigo')
+plt.tick_params(axis='y', labelcolor='indigo')
+plt.legend(loc='upper right')
+
+# 두 번째 서브플롯 설정(막대 그래프)
+plt.subplot(2, 1, 2)
+plt.bar(x, y2, color='deeppink', alpha=0.7, width=0.5, lable='Survived')
+plt.xlabel('Parch')
+plt.ylabel('Survived Count', color='deeppink')
+plt.tick_params(axis='y', labelcolor='deeppink')
+plt.legend(loc='upper right')
+
+# 제목 설정
+plt.suptitle('Survival Analysis by Number of Parents/Children (Plarch) on the Titanic')
+plt.tight_layout()
+plt.savefig('Figure13.png')
+plt.close()
+
+## **개별 서브플롯을 동시에 생성하기**
+
+# 2*2 구성의 figure
+fig, axes = plt.subplots(2, 2)
+
+# 선그래프
