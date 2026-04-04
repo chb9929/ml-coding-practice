74986b2a0f4c368253e2318177352e376d8d748c
최현빈 <chb9929@sunmoon.ac.kr>
2026-04-04 13:25:15 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -50,6 +50,31 @@ print(housing_num.median().values)  # 수동으로 계산한 중간값
 X = imputer.transform(housing_num)
 
 imputer.feature_names_in_
+
 housing_tr = pd.DataFrame(X, columns=housing_num.columns,
                           index=housing_num.index)
+housing_tr.loc[null_rows_idx].head()
 
+# 이상치 삭제
+from sklearn.ensemble import IsolationForest
+
+isolation_forest = IsolationForest(random_state=42)
+outlier_pred = isolation_forest.fit_predict(X)
+
+outlier_pred
+
+housing = housing.iloc[outlier_pred == 1]
+housing_labels = housing_labels.iloc[outlier_pred == 1]
+
+# 텍스트와 범주형 특성 다루기
+housing_cat = housing[["ocean_proximity"]]
+housing_cat.head(8)
+
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+
+housing_cat_encoded[:8]
+
+ordinal_encoder.categories_
