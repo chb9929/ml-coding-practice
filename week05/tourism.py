1c9f5e4ef6999dedbf5b8cf8253681b789fb02ed
최현빈 <chb9929@sunmoon.ac.kr>
2026-04-11 15:56:37 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -31,10 +31,11 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
     jsonResult = []
     result = []
     
-    for year in range(nStartYear, nEndYear+1):
+    for year in range(nStartYear, nEndYe13ar+1):
         for month in range(1, 13):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
             jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)       #[CODE 2]
+            print("서버 응답 확인:", jsonData)
             if (jsonData['response']['header']['resultMsg'] == 'OK'):
                 #데이터가 없는 마지막 항목인 경우 --------------------------
                 if jsonData['response']['body']['items'] == '':
