b4fbd7b23bff0d07c40dec53fc667b9fb189e737
최현빈 <chb9929@sunmoon.ac.kr>
2026-04-11 14:57:26 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -37,4 +37,57 @@ def main():
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
 
-def
+def getNaverSearch(node, srcText, page_start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)                #[CODE 1]
+
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+    
+
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
+
+    try:
+        response = urllib.request.Request(req)
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+    
+
+def getPostData(post, jsonResult, cnt):   #[CODE 3]
+    title = post['title']
+    description = post['description']
+    org_link = post['originallink']
+    link = post['link']
+
+    # %a: 짧은 형식의 요일 이름 (예, 'Mon', 'Tue', 'Wed', ... )
+    # %d: 일 (예, 01, 02, 03, ...)
+    # %b: 짧은 형식의 월 이름 (예, 'Jan', 'Feb', 'Mar', ...)
+    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
+    pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+
+    jsonResult.append({
+        'cnt': cnt,
+        'title': title,
+        'description': description,
+        'org_link': org_link,
+        'link': link,
+        'pDate': pDate
+    })
+    return
+
+main()
