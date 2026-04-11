cdb4a344a1efb11aa08b1bfa9df09f9d83a649ed
최현빈 <chb9929@sunmoon.ac.kr>
2026-04-11 14:44:26 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -20,4 +20,7 @@ def main():
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, jsonResult, cnt)                # [CODE 3}]
+            getPostData(post, jsonResult, cnt)                # [CODE 3]
+
+        start = jsonResponse['start'] + jsonResponse['display']
+        
