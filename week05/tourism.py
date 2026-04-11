84bd05164f1e55394090d53afcf44a324f47da4e
최현빈 <chb9929@sunmoon.ac.kr>
2026-04-11 14:36:25 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -66,11 +66,25 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
 
     url = service_url + parameters
 
-    responseDecdoe = getRequestUrl(url)
+    responseDecdoe = getRequestUrl(url)                       #[CODE 1]
 
     if (responseDecdoe == None):
         return None
     else:
         return json.loads(responseDecdoe)
     
+"""### [CODE 1]"""
+
+def getRequestUrl(url):    #[CODE 1]
+    req = urllib.request.Request(url)
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
     
+main()
