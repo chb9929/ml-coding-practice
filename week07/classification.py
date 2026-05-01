c99f9f8fe6970e44ec73203b7684b151be591c86
최현빈 <chb9929@sunmoon.ac.kr>
2026-05-01 12:02:15 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -93,4 +93,10 @@ roc_auc_score(y_train_5, y_scores)
 #다음은 실행하는데 몇 분이 걸릴 수 있음
 from sklearn.ensemble import RandomForestClassifier
 
-forest_clf = 
+forest_clf = RandomForestClassifier(random_state=42)
+
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    method="predict_proba")
+
+y_scores_forest = y_probas_forest[:, 1]
+y_train_pred_forest = y_probas_forest[:, 1] >= 0.5  # 양성 확률 > 50%
