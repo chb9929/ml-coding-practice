d5f0d24864a690aa687ecc2efbca26be2ead695f
최현빈 <chb9929@sunmoon.ac.kr>
2026-05-04 17:20:21 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -217,3 +217,11 @@ wordcloud = WordCloud(background_color = 'white', width = 1400, height = 1400,
 
 plt.suptitle('Keywords in the description of Movies and TV shows',
              fontweight='bold', fontfamily='serif', fontsize=15)
+
+# 워드 클라우드 표시
+# plt.imshow( ) : array에 색을 채워서 이미지로 펴시
+plt.imshow(wordcloud)
+
+# 축 감추기
+plt.axis('off')
+plt.show()
