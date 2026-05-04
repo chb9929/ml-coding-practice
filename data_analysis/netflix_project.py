1ed99f914b99eaa4965036e1366c42ac130027b4
최현빈 <chb9929@sunmoon.ac.kr>
2026-05-04 16:33:11 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -178,4 +178,24 @@ netflix_age_country_unstack
 netflix_age_country_unstack = netflix_age_country_unstack.div(netflix_age_country_unstack.sum(axis=0), axis=1)
 netflix_age_country_unstack
 
-plt.
+plt.figure(figsize=(15, 5))
+
+# 사용자 정의 컬러맵 만들기
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710','#f5f5f1'])
+
+sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidths=2.5, annot=True, fmt='.0%')
+
+plt.suptitle('Target ages proportion of total content by country',
+             fontweight='bold', fontfamily='serif', fontsize=15)
+plt.title('Here we see interesting differences between countries. Most shows in South Korea are targeted to adults, for instance.',
+          fontsize=12. fontfamily='serif')
+plt.show()
+
+# 넷플릭스 데이터의 description 열 이용한 워드 클라우드 생성
+
+# WordCloud : 워드 클라우드 생성에 필요한 모듈
+# Image : 워드 클라우드를 원하는 형태로 그리기 위해 그림을 불러오는 패키지
+from wordcloud import WordCloud
+from PIL import Image
+
+plt.figure(figsize=(15, 5))
