eb288b9bc730c7f24dd700605138329b55d64b63
최현빈 <chb9929@sunmoon.ac.kr>
2026-05-10 14:13:49 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -69,4 +69,50 @@ lin_reg = LinearRegression()
 lin_reg.fit(X_poly, y)
 lin_reg.intercept_, lin_reg.coef_
 
-X_new
+X_new = np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$", rotation=0)
+plt.legend(loc="upper left")
+plt.axis([-3, 3, 0, 10])
+plt.grid()
+plt.show()
+
+# 릿지 회귀
+
+np.random.seed(42)
+m = 20
+X = 3* np.random.rand(m, 1)
+y = 1+ 0.5 * X + np.random.randn(m, 1) / 1.5
+X_new = np.linspace(0, 3, 100).reshape(100, 1)
+
+# 방금 생성한 데이터셋을 간단히 살펴보기
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, ".")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$  ", rotation=0)
+plt.axis([0, 3, 0, 3.5])
+plt.grid()
+plt.show()
+
+from sklearn.linear_model import Ridge
+
+ridge_reg = Ridge(alpha=0.1, solver="cholesky")
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+
+sgd_reg = SGDRegressor(penalty="12", alpha=0.1 / m, tol=None,
+                       max_iter=1000, eta0=0.01, random_state=42)
+sgd_reg.fit(X, y.ravel())   # fit()은 1D 타겟을 기대하므로 y.ravel()을 사용합니다.
+print(sgd_reg.predict([[1.5]]))
+
+# 확률적 평균 GD(solver="sag")를 사용하면
+# 이전과 거의 동일한 솔루션을 얻을 수 있음을 보여줍니다.
+ridge_reg = Ridge(alpha=0.1, solver="sag", random_state=42)
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([1.5]))
