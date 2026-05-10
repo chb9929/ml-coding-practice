3f1ac00ff5487b9e58dda1fcbed4961edc473557
최현빈 <chb9929@sunmoon.ac.kr>
2026-05-10 14:36:19 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -44,4 +44,7 @@ poly_kernel_svm_clf.fit(X, y)
 
 # 가우스 RBF 커널
 rbf_kernel_svm_clf = make_pipeline(StandardScaler(),
-                                   SVC(kernel="rbf", ))
+                                   SVC(kernel="rbf", gamma=5, C=0.001))
+rbf_kernel_svm_clf.fit(X, y)
+
+# SVM 회귀
