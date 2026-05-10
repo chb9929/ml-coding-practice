481cafccffa8c358ae0d273a7001dbf8c813b381
최현빈 <chb9929@sunmoon.ac.kr>
2026-05-10 14:18:19 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -121,3 +121,33 @@ print(ridge_reg.predict([1.5]))
 
 from sklearn.linear_model import Lasso
 
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+print(lasso_reg.predict([[1.5]]))
+
+# 엘라스틱넷
+
+from sklearn.linear_model import ElasticNet
+
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+print(elastic_net.predict([[1.5]]))
+
+# 로지스틱 회귀
+from sklearn.datasets import load_iris
+
+iris = load_iris(as_frame=True)
+list(iris)
+
+print(iris.data.head(3))
+print(iris.target.head(3))   # 샘플이 섞여 있지 않습니다.
+
+print(iris.target_names)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+X = iris.data[["petal width (cm)"]].values
+y = iris.target_names[iris.target] == 'virginica'
+X_train, X_test, y_train, y_test = train__test_split(X, y, random_state=42)
+
