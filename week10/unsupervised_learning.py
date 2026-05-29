81ddc92d4e6cb060e8de3a5f7d4953534666f1b5
최현빈 <chb9929@sunmoon.ac.kr>
2026-05-29 13:42:02 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -64,6 +64,8 @@ plt.show()
 
 from sklearn.metrics import silhouette_score
 
+print(silhouette_score(X, kmeans.labels_))
+
 silhouette_scores = [silhouette_score(X, model.labels_) for model in kmeans_per_k[1:]]
 
 plt.figure(figsize=(8, 3))
